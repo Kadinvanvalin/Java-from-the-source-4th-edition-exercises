892b1e384ddc90e62b306fe9607c2a488eb676f2
kadinvanvalin <kadinvanvalin@yahoo.com>
2017-11-25 20:40:03 -0500

    add string constant to HelloWorld

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,6 +1,7 @@
 public class HelloWorld {
+    final static String GREETING = "Hello world!";
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        System.out.println(GREETING);
 
     }
 }
