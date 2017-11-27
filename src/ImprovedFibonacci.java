891585b38ebef7f2434f367081593476fdea234d
kadinvanvalin <kadinvanvalin@yahoo.com>
2017-11-27 10:36:15 -0500

    Change loop to make i count backwards

--- src/ImprovedFibonacci.java
+++ src/ImprovedFibonacci.java
@@ -9,13 +9,13 @@ public class ImprovedFibonacci {
         int lo = 1, hi = 1;
         String mark;
 
-        System.out.println("1: " + lo);
-        for (int i = 2; i <= MAX_INDEX; i++) {
+        System.out.println("9: " + lo);
+        for (int i = MAX_INDEX - 1; i >= 1; i--) {
             if (hi % 2 == 0)
                 mark = " *";
             else
                 mark = "";
-            System.out.println(i + " :" + hi + mark);
+            System.out.println(i + ": " + hi + mark);
             hi = lo + hi;
             lo = hi - lo;
         }
